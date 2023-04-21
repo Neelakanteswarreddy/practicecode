b9843e0f78182f8af9acc5d10045f56a86e4a16b
Neelakanteswarreddy <neelakanteswarreddy0205@gmail.com>
2023-04-21 19:35:12 +0530

    code updated

--- Number.java
+++ Number.java
@@ -1,5 +1,13 @@
 class Number{
- public static printsubtraction(int a, int b){
+public static void main(String[] args)
+{
+  int a=45;
+  int b=54;
+  int v=printsubtraction(a,b);
+  System.out.println(v);
+}
+
+ public static int  printsubtraction(int a, int b){
    retrun a-b;
 }
 }
